72d1540079d06be604ce18ea5afed2086dbdae3c
Scott Sickles <scott.sickles@outlook.com>
2019-11-05 15:39:24 -0500

    Initial ProxyService implementation to successfully complete specification

--- src/test/scala/com/github/ssjam/ProxyServiceSpec.scala
+++ src/test/scala/com/github/ssjam/ProxyServiceSpec.scala
@@ -4,7 +4,13 @@ class ProxyServiceSpec extends UnitSpec {
   "A Proxy Service" when {
     "initializing" should {
       "accept a port number, p to listen for connections" in {
-        fail("not yet implemented")
+        val a = Array("-p", "12345")
+        val ps = new ProxyServer(12345)
+        ps.status() must equal(12345)
+      }
+      "set the port number to a default value" in {
+        val ps2 = new ProxyServer()
+        ps2.status() must equal(5978)
       }
     }
   }

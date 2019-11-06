1aefd6825ae424c460d632f0fb85704c98ccd812
Scott Sickles <scott.sickles@outlook.com>
2019-11-05 23:38:25 -0500

    added connection requirements

--- src/test/scala/com/github/ssjam/ProxyServiceSpec.scala
+++ src/test/scala/com/github/ssjam/ProxyServiceSpec.scala
@@ -2,6 +2,7 @@ package com.github.ssjam
 
 class ProxyServiceSpec extends UnitSpec {
   "A Proxy Service" when {
+
     "initializing" should {
       "accept a port number, p to listen for connections" in {
         val a = Array("-p", "12345")
@@ -11,6 +12,15 @@ class ProxyServiceSpec extends UnitSpec {
       "set the port number to a default value" in {
         val ps2 = new ProxyServer()
         ps2.status() must equal(5978)
+      }
+    }
+
+    "running" should {
+      "accept open connections" in {
+        fail("not yet implemented")
+      }
+      "accept TLS connections" in {
+        fail("not yet implemented")
       }
     }
   }
